9698bb51785a701fe61b7e658d4bb84fd2772ce7
Ananya Singh <as2863@njit.edu>
2020-06-29 20:17:17 -0400

    generateKeyPair  method used

---
+++ index.js
@@ -0,0 +1,21 @@
+const openssl = require('openssl-nodejs');
+const { generateKeyPair } = require('crypto');
+
+generateKeyPair('rsa', {
+  modulusLength: 2048,
+  publicKeyEncoding: {
+    type: 'spki',
+    format: 'pem'
+  },
+  privateKeyEncoding: {
+    type: 'pkcs8',
+    format: 'pem',
+    cipher: 'aes-256-cbc',
+    passphrase: 'top secret'
+  }
+}, (err, publicKey, privateKey) => {
+  // Handle errors and use the generated key pair.
+  if(err) throw err;
+  console.log(publicKey);
+  console.log(privateKey);
+});
